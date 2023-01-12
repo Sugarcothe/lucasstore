269b0794fc8693cc1bd2d64a8d107445435facfb
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-01-12 16:48:36 +0100

    stripe config

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useSelector } from "react-redux";
+import { removeItem, resetCart } from "../../redux/cartReducer";
+import { useDispatch } from "react-redux";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
+
+const Cart = () => {
+  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
+
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_51JKBeZR5ZnfAKxjoYODd0Nncl658nZd0xN4R9qGAmQbqo8DkBHNnpLweC5rGhS0UmL8vy7HSVJrd3nloEHjYe98m00KHSerCqA"
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">
+              {item.quantity} x ${item.price}
+            </div>
+          </div>
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>${totalPrice()}</span>
+      </div>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import "./Slider.scss";
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const data = [
+    "assets/shoes.jpg",
+    "assets/slider10.jpg",
+    "assets/slider4.jpg",
+  ];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
+
+  return (
+    <div className="slider">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <EastOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
